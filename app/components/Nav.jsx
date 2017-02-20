942de2636246505733db13078ff73aad7ed963c2
Nepesh Pradhan <nepeshpradhan@Nepesh.local>
2017-02-20 21:55:44 +1300

    props and refs

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -4,7 +4,13 @@ var {Link, IndexLink} = require('react-router');
 var Nav = React.createClass({
   onSearch: function(e){
     e.preventDefault();
-    alert('not yet wired up');
+    var location = this.refs.location.value;
+
+    var encodedLocation = encodeURIComponent(location);
+    if (location && location.length>0){
+      this.refs.location.value = '';
+      window.location.hash  ='#/?location='+encodedLocation;
+    }
   },
   render: function(){
     return (
@@ -30,7 +36,7 @@ var Nav = React.createClass({
           <form onSubmit={this.onSearch}>
             <ul className="menu">
               <li>
-                <input type="search" placeholder="Search weather" placeholder="Search Weather by City"/>
+                <input type="search" ref="location" placeholder="Search weather" placeholder="Search Weather by City"/>
               </li>
               <li>
                 <input type="submit" className="button" value="Get Weather"/>
